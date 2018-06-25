d2993a9a70ab474b6187d42ccc088fa275ed590f
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-06-25 13:29:12 +0700

    add time-now.php

---
+++ time-now.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+	<!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+    <div class="wrapper">
+
+
+<div class="container py-5">
+  <div class="row">
+      <div class="col-4">
+                    <div class="card card-all mb-2">
+                      <div class="show-product-fix-img">
+                        <a href="car-details.php">
+                          <img class="card-img-top border-radius-none" src="assets/images/car/c23afbda3cb7df88c914574fe68ce1d2.jpg" alt="Card image cap">
+                        </a>
+                        <div class="time-slae-card">
+                           <div class="time-slae-text">พิเศษ <i class="flaticon-percentage"></i></div>
+                        </div>
+                      </div>
+                      <div class="card-body px-0">
+                        <div class="card-head-content">
+                          <h5 class="card-title text-purple">2016 Toyota Hilux Revo 2.4 Prerunner G Pickup</h5>
+                        </div>
+                        <div class="card-content">
+                          <p class="card-text">สภาพยอดเยี่ยม รถไม่เคยชน เช็คสภาพรถตามระยะ สภาพยางดี</p>
+                        </div>
+                      </div>
+                      <div class="card-footer card-footer-all px-0 d-flex">
+                        <div class="text-purple mr-auto">
+                          <label>279,000 บาท</label>
+                        </div>
+                        <div class="checkbox-compare">
+                          <label class="check-compare">เปรียบเทียบ
+                            <input type="checkbox" checked="checked">
+                            <span class="checkmark-show-product"></span>
+                          </label>
+                        </div>
+                        <div class="wishlist">
+                          <div class="wish-position">
+                            <label class="star-wish">
+                              <i  onclick="myFunction(this)" class="fa fa-star-o" aria-hidden="true"></i>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card-body bg-gray p-3">
+                        <ul class="show-list-detail">
+                          <li class="list-item">
+                            <i class="fa flaticon-speedometer" aria-hidden="true"></i>
+                            <label class="text-contact">120 - 125k กม.</label>
+                          </li>
+                          <li class="list-item">
+                            <i class="fa flaticon-gearbox"></i>
+                            <label class="text-contact"> เกียร์อัตโนมัติ</label>
+                          </li>
+                          <li class="list-item">
+                            <i class="fa flaticon-map-pin-marked"></i>
+                            <label class="text-contact">กรุงเทพมหานคร</label>
+                          </li>
+                          <li class="list-item">
+                            <i class="fa flaticon-manager"></i>
+                            <label class="text-contact">รถบ้าน</label>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="card-footer bg-white px-0 d-md-flex flex-md-row">
+                        <div>
+                           <a href="#" class="btn-purple btn-small skip">
+                            <span class="hover-bg reset">
+                              <span class="hover-text reset"><label class="text-btn-purple text-medium"><i class="fa fa-phone mr-1" aria-hidden="true"></i>ติดต่อผู้ขาย</label></span>
+                            </span>
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                          </a>
+                        </div>
+                        <div class="ml-auto">
+                          <a href="#" class="btn-yellow btn-small skip">
+                            <span class="hover-bg reset">
+                              <span class="hover-text reset"><label class="text-btn-yellow text-medium">จอง</label></span>
+                            </span>
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div> 
+      </div>
+  </div>
+
+</div>
+
+
+
+    </div>
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
